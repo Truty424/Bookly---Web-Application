13e907c68a325ab6e57e99b2979c028a234f0498
amirrezajm <amirreza.jolani@yahoo.com>
2025-03-29 11:01:16 +0100

    forget to push author data classes(resource)

---
+++ bookly/src/main/java/it/unipd/bookly/Resource/Author.java
@@ -0,0 +1,64 @@
+package it.unipd.bookly.Resource;
+
+public class Author {
+    private int author_id;
+    private String first_name;
+    private String last_name;
+    private String biography;
+    private String nationality;
+
+    public Author(int author_id, String first_name, String last_name, String biography, String nationality) {
+        this.author_id = author_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.biography = biography;
+        this.nationality = nationality;
+    }
+
+    public Author(String first_name, String last_name, String biography, String nationality) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.biography = biography;
+        this.nationality = nationality;
+    }
+
+    public int getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(int author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}
