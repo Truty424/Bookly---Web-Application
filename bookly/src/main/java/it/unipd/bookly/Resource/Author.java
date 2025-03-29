b5a650621fe84e1682956ded7a388f01a219227e
amirrezajm <amirreza.jolani@yahoo.com>
2025-03-29 16:53:09 +0100

    fixed author code

--- bookly/src/main/java/it/unipd/bookly/Resource/Author.java
+++ bookly/src/main/java/it/unipd/bookly/Resource/Author.java
@@ -30,19 +30,19 @@ public class Author {
         this.author_id = author_id;
     }
 
-    public String getFirst_name() {
+    public String getFirstName() {
         return first_name;
     }
 
-    public void setFirst_name(String first_name) {
+    public void setFirstName(String first_name) {
         this.first_name = first_name;
     }
 
-    public String getLast_name() {
+    public String getLastName() {
         return last_name;
     }
 
-    public void setLast_name(String last_name) {
+    public void setLastName(String last_name) {
         this.last_name = last_name;
     }
 
