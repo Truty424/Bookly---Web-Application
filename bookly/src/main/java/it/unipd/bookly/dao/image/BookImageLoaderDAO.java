b175b46767ba8458fdbba55f38421c7074b10301
amirrezajm <amirreza.jolani@yahoo.com>
2025-03-31 10:28:35 +0200

    add sevlet files to the project but not complete

--- bookly/src/main/java/it/unipd/bookly/dao/image/BookImageLoaderDAO.java
+++ bookly/src/main/java/it/unipd/bookly/dao/image/BookImageLoaderDAO.java
@@ -10,18 +10,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BookImageLoaderDAO extends AbstractDAO<Image> {
-    private static final String STATEMENT_LOAD_RECIPE_IMAGE = "SELECT * FROM booklySchema.book_image WHERE recipe_id = ?";
-    private final int recipeId;
+    private static final String STATEMENT_LOAD_RECIPE_IMAGE = "SELECT * FROM booklySchema.book_image WHERE book_id = ?";
+    private final int bookId;
 
-    public BookImageLoaderDAO(final Connection con, int recipeId) {
+    public BookImageLoaderDAO(final Connection con, int bookId) {
         super(con);
-        this.recipeId = recipeId;
+        this.bookId = bookId;
     }
 
     @Override
     protected void doAccess() throws SQLException {
         try (PreparedStatement pstmt = con.prepareStatement(STATEMENT_LOAD_RECIPE_IMAGE)) {
-            pstmt.setInt(1, recipeId);
+            pstmt.setInt(1, bookId);
             try(ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next())
                     this.outputParam = new Image(rs.getBytes("image"), rs.getString("image_type"));
