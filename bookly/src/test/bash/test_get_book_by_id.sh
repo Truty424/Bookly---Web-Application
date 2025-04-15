7da200316b8f1c82149867f558848399c39190ce
amirrezajm <amirreza.jolani@yahoo.com>
2025-04-15 19:19:00 +0200

    remove bash and order test

--- bookly/src/test/bash/test_get_book_by_id.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BASE_URL="http://localhost:8080"
-BOOK_ID="1"
-
-curl -i -X GET "${BASE_URL}/api/book?id=${BOOK_ID}"
--- bookly/src/test/bash/test_get_orders_by_user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BASE_URL="http://localhost:8080"
-USER_ID=5
-
-curl -i -X GET "${BASE_URL}/api/order/user/${USER_ID}"
